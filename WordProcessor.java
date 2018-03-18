fc38c4ddd2ea4544dd5a13ccabebe41ce141cf57
Giedre Stulgyte <2285161s@student.gla.ac.uk>
2018-03-18 23:28:01 +0000

    progress

--- WordProcessor.java
+++ WordProcessor.java
@@ -1,18 +1,18 @@
-
 //import classes for file input - scanner etc.
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 import java.util.Set;
-//import implementing set (eg. TreeSet)
+import java.util.TreeSet;
 
-
-
-
-public class WordProcessor {
-	private static <E> String displaySet(Set<E> inputSet){
+public class WordProcessor 
+{
+	private static <E> String displaySet(Set<E> inputSet)
+	{
 		//implement this static method to create a
 		// String representation of set - 5 comma separated elements per line
 		// assume that type E has a toString method
-		
+
 		return ""; // placeholder
 	}
 
@@ -20,19 +20,57 @@ public class WordProcessor {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		//create a set of type String called wordSet
-		//create a set of type CountedElement<String> called countedWordSet 
-		
-		//for each input file (assume 3 arguments, each the name of a file)
-		//  for each word w
-		//     if wordset doesnt contain w:
-		//        add w to wordset
-	    //        add new element to countedWordSet
-		//     else
-		//        increment numeric part of element in countedWordSet containing w
-		
 
-	// need this: System.out.println(displaySet(countedWordSet));
+		FileReader reader;
+		Scanner in;
+		String w;  
+
+		Set<String> wordSet = new TreeSet<String>();
+		Set<CountedElement<String>> countedWordSet = new TreeSet<CountedElement<String>>();
+
+		for (String fileName: args) 
+		{	
+			try 
+			{	
+				reader = new FileReader(fileName);
+				in = new Scanner (reader);
+
+				while (in.hasNextLine()) 
+				{
+					w = in.nextLine();
+
+					boolean found = false; 	
+					for (CountedElement<String> countedElement: countedWordSet) 	
+					{
+						if(countedElement.getElement().equals(w)) {
+							int c = countedElement.getCount(); 
+							countedElement.setCount(c++);
+							found = true;
+							break;
+						}
+					}
+
+					if (!found)
+					{
+						wordSet.add(w);
+						CountedElement<String> cElement = new CountedElement<String>(w);
+
+						wordSet.add(w);
+						countedWordSet.add(cElement);
+					}
+
+				}
+
+				in.close(); 
+			}
+
+			catch (FileNotFoundException e) {
+				System.out.println("File not found");
+			}
+
+		}
+
+		System.out.println(displaySet(countedWordSet));
 
 	}
 }
